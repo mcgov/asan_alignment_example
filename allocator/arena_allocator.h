6d3874ec0886533cb6fad37d21dbfcb8692cbca4
Matthew G McGovern <mamcgove@microsoft.com>
2020-12-11 09:46:50 -0800

    undo std::aligned_storage debacle

--- allocator/arena_allocator.h
+++ allocator/arena_allocator.h
@@ -3,17 +3,28 @@
 #include <memory>
 #include <new>      // bad_alloc, bad_array_new_length
 #include <stdlib.h> //size_t, malloc, free
+#include <stdint.h>
 
 // NOTE: This is a non-thread-safe, completely contrived example to
 //  illustrate ASan's alignment requirements. Do not attempt to use
 //  this for anything other than this example!
 
 // The allocator will have a fixed size and impose the correct padding restrictions.
-    typedef std::aligned_storage<sizeof(size_t),16>::type aligned_t;
-    constexpr size_t ArenaSize = 0x1000;
-    aligned_t Arena[ArenaSize];
-    size_t current_index = 1; //start one object inside the arena.
+    constexpr size_t ArenaSize = 0x1000 * sizeof(size_t);
+    static uint8_t *Arena = (uint8_t*) _aligned_malloc(ArenaSize, 0x10);
+    size_t cursor = 0x10; //start 16 bytes inside the arena
+    size_t padding_size = 0x10; //add (at least) a qword of padding per allocation.
 
+// a function to calculate the padding needed per allocation.
+// Adding padding per element breaks array iteration (in this scenario)
+// So we will only pad the ends of each array that is allocated.
+size_t CalculateSizePlusPadding(size_t allocation_size, size_t padding){
+  size_t combined_size = allocation_size + padding;
+  if (combined_size % 8 != 0) {
+    return combined_size + (8 - (combined_size % 8)); // pad to a qword boundary.
+  }
+  return combined_size;
+}
 
 // This example allocator is based on the std::allocator interface,
 // the example is also basically stolen from the 'mallocator' example
@@ -25,8 +36,7 @@ template <class T> struct ArenaAllocator {
   ArenaAllocator() noexcept {
     ASAN_POISON_MEMORY_REGION(Arena, ArenaSize);
   }
-
-  // A converting copy constructor:
+  // std::allocator members
   template <class U> ArenaAllocator(const ArenaAllocator<U> &) noexcept {}
   template <class U> bool operator==(const ArenaAllocator<U> &) const noexcept {
     return true;
@@ -45,22 +55,18 @@ template <class T> T *ArenaAllocator<T>::allocate(const size_t n) const {
   if (n > ArenaSize) {
     throw std::bad_array_new_length();
   }
-  if (current_index + n >= ArenaSize) {
+  if (cursor + n >= ArenaSize) {
     throw std::bad_alloc();
   }
-  // poison each element, note we're unpoisoning sizeof(T) 
-  // not alignof(T), we want to leave padding poisoned.
-  for (int i = 0; i < n; i++){
-      void *each_alloc = &Arena[current_index+i];
-      ASAN_UNPOISON_MEMORY_REGION(each_alloc, sizeof(T));
-  }
-  void* returned_alloc = &Arena[current_index];
-  current_index += n;
-  return static_cast<T *>(returned_alloc);
+  
+  void *new_alloc = (void*) &Arena[cursor];   
+  size_t allocation_size = sizeof(T) * n;
+  ASAN_UNPOISON_MEMORY_REGION(new_alloc, allocation_size);
+  cursor += CalculateSizePlusPadding(allocation_size, padding_size);
+  return static_cast<T *>(new_alloc);
 }
 
 template <class T>
 void ArenaAllocator<T>::deallocate(T *const p, size_t s) const noexcept {
-  // repoison the entire region
   ASAN_POISON_MEMORY_REGION(p, s * alignof(T));
 }
--- allocator/main.cpp
+++ allocator/main.cpp
@@ -3,12 +3,16 @@
 int main () {
     
     ArenaAllocator<size_t> myAllocator;
-    
+
     size_t *one = myAllocator.allocate(1);
     size_t *two = myAllocator.allocate(2);
     size_t *three = myAllocator.allocate(3);
     size_t *four = myAllocator.allocate(4);
 
     two[2] = 0xFF; // trigger an ASan report.
-    return 1;
+    // the report will show the shadow memory layout.
+    // comment out line 12 to observe a deallocated access.
+
+    myAllocator.deallocate(two, 2);
+    two[0] = 0xFF; // trigger another ASan report.
 }
